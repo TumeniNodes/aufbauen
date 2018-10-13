5b5e8c33ce7affcd7eaa5e62d81b31252f69d7bf
TumeniNodes <tumeninodes@gmail.com>
2018-10-13 14:29:49 -0400

    finish stone migration

--- mods/stone/stone.lua
+++ mods/stone/stone.lua
@@ -43,15 +43,6 @@ minetest.register_node("stone:cobblestone_mossy", {
 -- Granite Nodes
 --
 
-minetest.register_node("stone:granite", {
-	description = "Granite",
-	tiles = {"stone_granite.png"},
-	groups = {cracky = 3, stone = 1},
-	drop = {},
---	legacy_mineral = true,
-	sounds = default.node_sound_stone_defaults(),
-})
-
 minetest.register_node("stone:granite_block", {
 	description = "Granite Block",
 	tiles = {"stone_granite_block.png"},
@@ -84,14 +75,6 @@ minetest.register_node("stone:granite_smooth", {
 --
 -- Limestone Nodes
 --
-
-minetest.register_node("stone:limestone", {
-	description = "Limestone",
-	tiles = {"stone_limestone.png"},
-	groups = {crumbly = 1, cracky = 3},
-	drop = {},
-	sounds = default.node_sound_stone_defaults(),
-})
 
 minetest.register_node("stone:limestone_brick", {
 	description = "Limestone Brick",
@@ -137,6 +120,160 @@ minetest.register_node("stone:limestone_smooth", {
 	tiles = {"stone_limestone_smooth.png"},
 	is_ground_content = false,
 	groups = {cracky = 2},
+	drop = {},
+	sounds = default.node_sound_stone_defaults(),
+})
+
+
+--
+-- Sandstone Nodes
+--
+
+minetest.register_node("stone:sandstone_brick", {
+	description = "Sandstone Brick",
+	paramtype2 = "facedir",
+	place_param2 = 0,
+	tiles = {"stone_sandstone_brick.png"},
+	is_ground_content = false,
+	groups = {cracky = 2},
+	drop = {},
+	sounds = default.node_sound_stone_defaults(),
+})
+
+minetest.register_node("stone:sandstone_block", {
+	description = "Sandstone Block",
+	tiles = {"stone_sandstone_block.png"},
+	is_ground_content = false,
+	groups = {cracky = 2},
+	drop = {},
+	sounds = default.node_sound_stone_defaults(),
+})
+
+minetest.register_node("stone:sandstone_block_half", {
+	description = "Sandstone Block Half",
+	tiles = {"stone_sandstone_block_half.png"},
+	is_ground_content = false,
+	paramtype2 = "facedir",
+	groups = {cracky = 2},
+	drop = {},
+	sounds = default.node_sound_stone_defaults(),
+})
+
+minetest.register_node("stone:sandstone_cinderblock", {
+	description = "Sandstone Cinderblock",
+	tiles = {"stone_sandstone_cinderblock.png"},
+	is_ground_content = false,
+	groups = {cracky = 2},
+	drop = {},
+	sounds = default.node_sound_stone_defaults(),
+})
+
+minetest.register_node("stone:sandstone_smooth", {
+	description = "Smooth Sandstone",
+	tiles = {"stone_sandstone_smooth.png"},
+	is_ground_content = false,
+	groups = {cracky = 2},
+	drop = {},
+	sounds = default.node_sound_stone_defaults(),
+})
+
+
+--
+-- Stone Nodes
+--
+
+minetest.register_node("stone:stone_brick", {
+	description = "Stone Brick",
+	paramtype2 = "facedir",
+	place_param2 = 0,
+	tiles = {"stone_stone_brick.png"},
+	is_ground_content = false,
+	groups = {cracky = 2, stone = 1},
+	drop = {},
+	sounds = default.node_sound_stone_defaults(),
+})
+
+minetest.register_node("stone:stone_brick_mossy", {
+	description = "Mossy Stone Brick",
+	paramtype2 = "facedir",
+	place_param2 = 0,
+	tiles = {"stone_stone_brick_mossy.png"},
+	is_ground_content = false,
+	groups = {cracky = 2, stone = 1},
+	drop = {},
+	sounds = default.node_sound_stone_defaults(),
+})
+
+minetest.register_node("stone:stone_block", {
+	description = "Stone Block",
+	tiles = {"stone_stone_block.png"},
+	is_ground_content = false,
+	groups = {cracky = 2, stone = 1},
+	drop = {},
+	sounds = default.node_sound_stone_defaults(),
+})
+
+minetest.register_node("stone:stone_block_mossy", {
+	description = "Mossy Stone Block",
+	tiles = {"stone_stone_block_mossy.png"},
+	is_ground_content = false,
+	groups = {cracky = 2, stone = 1},
+	drop = {},
+	sounds = default.node_sound_stone_defaults(),
+})
+
+minetest.register_node("stone:stone_block_half", {
+	description = "Stone Block Half",
+	tiles = {"stone_stone_block_half.png"},
+	is_ground_content = false,
+	paramtype2 = "facedir",
+	groups = {cracky = 2, stone = 1},
+	drop = {},
+	sounds = default.node_sound_stone_defaults(),
+})
+
+minetest.register_node("stone:stone_block_half_mossy", {
+	description = "Mossy Stone Block Half",
+	tiles = {"stone_stone_block_half_mossy.png"},
+	is_ground_content = false,
+	paramtype2 = "facedir",
+	groups = {cracky = 2, stone = 1},
+	drop = {},
+	sounds = default.node_sound_stone_defaults(),
+})
+
+minetest.register_node("stone:stone_cinderblock", {
+	description = "Stone Cinder Block",
+	tiles = {"stone_stone_cinderblock.png"},
+	is_ground_content = false,
+	groups = {cracky = 2, stone = 1},
+	drop = {},
+	sounds = default.node_sound_stone_defaults(),
+})
+
+minetest.register_node("stone:stone_cinderblock_mossy", {
+	description = "Mossy Stone Cinder Block",
+	tiles = {"stone_stone_cinderblock_mossy.png"},
+	is_ground_content = false,
+	groups = {cracky = 2, stone = 1},
+	drop = {},
+	sounds = default.node_sound_stone_defaults(),
+})
+
+minetest.register_node("stone:stone_smooth", {
+	description = "Smooth Stone",
+	tiles = {"stone_stone_smooth.png"},
+	is_ground_content = false,
+	groups = {cracky = 2, stone = 1},
+	drop = {},
+	sounds = default.node_sound_stone_defaults(),
+})
+
+minetest.register_node("stone:stone_smooth_mossy", {
+	description = "Mossy Smooth Stone",
+	tiles = {"stone_stone_smooth_mossy.png"},
+	is_ground_content = false,
+	groups = {cracky = 2, stone = 1},
 	drop = {},
 	sounds = default.node_sound_stone_defaults(),
 })
