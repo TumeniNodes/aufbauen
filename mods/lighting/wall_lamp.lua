45dcc94c478f0f063266b85040b3f0b3aaa0c389
TumeniNodes <tumeninodes@gmail.com>
2018-11-17 19:30:44 -0500

    fix wall lamps and clean up code

--- mods/lighting/wall_lamp.lua
+++ mods/lighting/wall_lamp.lua
@@ -1,8 +1,10 @@
+-- Exterior Deco Wall Lamps
+
 -- Manual Wall Lamps
 
 minetest.register_node("lighting:wall_lamp_off", {
 	description = "Wall Lamp Off",
-    use_texture_alpha = true,
+	use_texture_alpha = true,
 	tiles = {
 		-- up, down, right, left, back, front
 		'lighting_streetlamp_top.png',
@@ -14,8 +16,8 @@ minetest.register_node("lighting:wall_lamp_off", {
 	},
 	drawtype = "nodebox",
 	paramtype = "light",
-    paramtype2 = "facedir",
---    legacy_facedir_simple = true,
+	paramtype2 = "facedir",
+--	legacy_facedir_simple = true,
 	sunlight_propagates = true,
 	groups = {snappy = 3},
 	sounds = default.node_sound_glass_defaults(),
@@ -39,22 +41,49 @@ minetest.register_node("lighting:wall_lamp_off", {
 
 minetest.register_node("lighting:wall_lamp_on", {
 	description = "Street Lamp ",
-    use_texture_alpha = true,
+	use_texture_alpha = true,
 	tiles = {
-		-- up, down, right, left, back, front
 		'lighting_streetlamp_top.png',
 		'lighting_streetlamp_top.png',
-		'lighting_wall_lamp_onR.png',
-		'lighting_wall_lamp_onL.png',
-		'lighting_streetlamp_on.png',
-		'lighting_streetlamp_on.png',
+		{image = 'lighting_wall_lamp_onR.png',
+		animation = {
+				type = "vertical_frames",
+				aspect_w = 16,
+				aspect_h = 16,
+				length = 4.0
+				}
+			},
+		{image = 'lighting_wall_lamp_onL.png',
+		animation = {
+				type = "vertical_frames",
+				aspect_w = 16,
+				aspect_h = 16,
+				length = 4.0
+				}
+			},
+		{image = 'lighting_streetlamp_on.png',
+		animation = {
+				type = "vertical_frames",
+				aspect_w = 16,
+				aspect_h = 16,
+				length = 4.0
+				}
+			},
+		{image = 'lighting_streetlamp_on.png',
+		animation = {
+				type = "vertical_frames",
+				aspect_w = 16,
+				aspect_h = 16,
+				length = 4.0
+				}
+			},
 	},
 	drawtype = "nodebox",
 	paramtype = "light",
-    paramtype2 = "facedir",
---    legacy_facedir_simple = true,
+	paramtype2 = "facedir",
+--	legacy_facedir_simple = true,
 	sunlight_propagates = true,
-	light_source = 12,
+	light_source = 6,
 	groups = {not_in_creative_inventory = 1},
 	node_box = {
 		type = "fixed",
@@ -74,37 +103,37 @@ minetest.register_node("lighting:wall_lamp_on", {
 	end,
 })
 
---[[    minetest.register_abm({
-        	nodenames = {"lighting:streetlamp_off", "lighting:streetlamp_on"},
-        	interval = 10,
-        	chance = 1,
---            catch_up = false,
-        	action = function(pos, node)
-        		local tod = minetest.get_timeofday()
-        		if tod >= 0.2 and tod <= 0.8 then
-        			if node.name == "lighting:streetlamp_on" then
-        				minetest.swap_node(pos, {name = "lighting:streetlamp_off"})
-        			end
-        		else
-        			if node.name == "lighting:streetlamp_off" then
-        				minetest.swap_node(pos, {name = "lighting:streetlamp_on"})
-        			else
-        				minetest.add_particle({
-        					pos = {x=pos.x, y=pos.y-0.1, z=pos.z},
-        					velocity = {x=0, y=0, z=0},
-        					acceleration = {x=0, y=0, z=0},
-        					expirationtime = 11,
-        					size = 20,
-        					collisiondetection = false,
-        					collisionremoval = false,
-        					vertical = true,
-        					texture = "lighting_streetlamp_glow.png",
-        					glow = 7
-        				})
-        			end
-        		end
-        	end
-        })]]--
+--[[	minetest.register_abm({
+			nodenames = {"lighting:streetlamp_off", "lighting:streetlamp_on"},
+			interval = 10,
+			chance = 1,
+			catch_up = false,
+			action = function(pos, node)
+				local tod = minetest.get_timeofday()
+				if tod >= 0.2 and tod <= 0.8 then
+					if node.name == "lighting:streetlamp_on" then
+						minetest.swap_node(pos, {name = "lighting:streetlamp_off"})
+					end
+				else
+					if node.name == "lighting:streetlamp_off" then
+						minetest.swap_node(pos, {name = "lighting:streetlamp_on"})
+					else
+						minetest.add_particle({
+							pos = {x=pos.x, y=pos.y-0.1, z=pos.z},
+							velocity = {x=0, y=0, z=0},
+							acceleration = {x=0, y=0, z=0},
+							expirationtime = 11,
+							size = 20,
+							collisiondetection = false,
+							collisionremoval = false,
+							vertical = true,
+							texture = "lighting_streetlamp_glow.png",
+							glow = 7
+						})
+					end
+				end
+			end
+})]]--
 
 
 
@@ -112,7 +141,7 @@ minetest.register_node("lighting:wall_lamp_on", {
 
 minetest.register_node("lighting:tod_wall_lamp_off", {
 	description = "TOD Wall Lamp Off",
-    use_texture_alpha = true,
+	use_texture_alpha = true,
 	tiles = {
 		-- up, down, right, left, back, front
 		'lighting_streetlamp_top.png',
@@ -124,8 +153,8 @@ minetest.register_node("lighting:tod_wall_lamp_off", {
 	},
 	drawtype = "nodebox",
 	paramtype = "light",
-    paramtype2 = "facedir",
---    legacy_facedir_simple = true,
+	paramtype2 = "facedir",
+--	legacy_facedir_simple = true,
 	sunlight_propagates = true,
 	groups = {snappy = 3},
 	sounds = default.node_sound_glass_defaults(),
@@ -140,27 +169,54 @@ minetest.register_node("lighting:tod_wall_lamp_off", {
 	selection_box = {
 		type = "fixed",
 		fixed = {-3/16, -1/2, 1/8, 3/16, 1/8, 1/2},
-    },
+	},
 })
 
 minetest.register_node("lighting:tod_wall_lamp_on", {
 	description = "TOD Wall Lamp ",
-    use_texture_alpha = true,
+	use_texture_alpha = true,
 	tiles = {
-		-- up, down, right, left, back, front
 		'lighting_streetlamp_top.png',
 		'lighting_streetlamp_top.png',
-		'lighting_wall_lamp_onR.png',
-		'lighting_wall_lamp_onL.png',
-		'lighting_streetlamp_on.png',
-		'lighting_streetlamp_on.png',
+		{image = 'lighting_wall_lamp_onR.png',
+		animation = {
+				type = "vertical_frames",
+				aspect_w = 16,
+				aspect_h = 16,
+				length = 4.0
+				}
+			},
+		{image = 'lighting_wall_lamp_onL.png',
+		animation = {
+				type = "vertical_frames",
+				aspect_w = 16,
+				aspect_h = 16,
+				length = 4.0
+				}
+			},
+		{image = 'lighting_streetlamp_on.png',
+		animation = {
+				type = "vertical_frames",
+				aspect_w = 16,
+				aspect_h = 16,
+				length = 4.0
+				}
+			},
+		{image = 'lighting_streetlamp_on.png',
+		animation = {
+				type = "vertical_frames",
+				aspect_w = 16,
+				aspect_h = 16,
+				length = 4.0
+				}
+			},
 	},
 	drawtype = "nodebox",
 	paramtype = "light",
-    paramtype2 = "facedir",
---    legacy_facedir_simple = true,
+	paramtype2 = "facedir",
+--	legacy_facedir_simple = true,
 	sunlight_propagates = true,
-	light_source = 12,
+	light_source = 6,
 	groups = {not_in_creative_inventory = 1},
 	node_box = {
 		type = "fixed",
@@ -176,33 +232,34 @@ minetest.register_node("lighting:tod_wall_lamp_on", {
 	},
 })
 
-    minetest.register_abm({
-        	nodenames = {"lighting:tod_wall_lamp_off", "lighting:tod_wall_lamp_on"},
-        	interval = 10,
-        	chance = 1,
---            catch_up = false,
-        	action = function(pos, node)
-        		local tod = minetest.get_timeofday()
-        		if tod >= 0.2 and tod <= 0.8 then
-        			if node.name == "lighting:tod_wall_lamp_on" then
-        				minetest.swap_node(pos, {name = "lighting:tod_wall_lamp_off", param2 = node.param2})
-        			end
-        		else
-        			if node.name == "lighting:tod_wall_lamp_off" then
-        				minetest.swap_node(pos, {name = "lighting:tod_wall_lamp_on", param2 = node.param2})
-        			    minetest.add_particle({
-        					pos = {x=pos.x, y=pos.y-0.1, z=pos.z+0.2},
-        					velocity = {x=0, y=0, z=0},
-        					acceleration = {x=0, y=0, z=0},
-        					expirationtime = 11,
-        					size = 20,
-        					collisiondetection = false,
-        					collisionremoval = false,
-        					vertical = true,
-        					texture = "lighting_streetlamp_glow.png",
-        					glow = 7
-        				})
-        			end
-        		end
-        	end
-        })
+--[[	minetest.register_abm({
+			nodenames = {"lighting:tod_wall_lamp_off", "lighting:tod_wall_lamp_on"},
+			interval = 10,
+			chance = 1,
+			catch_up = false,
+			action = function(pos, node)
+				local tod = minetest.get_timeofday()
+				if tod >= 0.2 and tod <= 0.8 then
+					if node.name == "lighting:tod_wall_lamp_on" then
+						minetest.swap_node(pos, {name = "lighting:tod_wall_lamp_off", param2 = node.param2})
+					end
+				else
+					if node.name == "lighting:tod_wall_lamp_off" then
+						minetest.swap_node(pos, {name = "lighting:tod_wall_lamp_on", param2 = node.param2})
+						minetest.add_particle({
+							pos = {x=pos.x, y=pos.y-0.1, z=pos.z+0.2},
+							velocity = {x=0, y=0, z=0},
+							acceleration = {x=0, y=0, z=0},
+							expirationtime = 0,
+							size = 20,
+							collisiondetection = false,
+							collisionremoval = false,
+							vertical = true,
+							texture = "lighting_streetlamp_glow.png",
+							glow = 6
+						})
+					end
+				end
+			end
+		})
+]]--
